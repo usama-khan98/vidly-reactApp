5f679f5eb418826510c286b054a8592ac3dcb187
usama khan <usama.alikhan98@gmail.com>
2021-02-16 05:00:54 +0000

    created separate table component

--- src/components/moviesTable.jsx
+++ src/components/moviesTable.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import Like from './common/like';
-import TableBody from './common/tableBody';
-import TableHeader from './common/tableHeader';
+import Table from './common/table';
 
 class MoviesTable extends Component {
 	columns = [
@@ -35,14 +34,12 @@ class MoviesTable extends Component {
 		const { movies, onSort, sortColumn } = this.props;
 
 		return (
-			<table className="table">
-				<TableHeader
-					columns={this.columns}
-					sortColumn={sortColumn}
-					onSort={onSort}
-				/>
-				<TableBody data={movies} columns={this.columns} />
-			</table>
+			<Table
+				columns={this.columns}
+				data={movies}
+				sortColumn={sortColumn}
+				onSort={onSort}
+			/>
 		);
 	}
 }
